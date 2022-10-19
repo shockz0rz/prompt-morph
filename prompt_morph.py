dcd29096e0d11999e969588a337437b2f1ab401d
shockz0rz <59752493+shockz0rz@users.noreply.github.com>
2022-10-19 16:32:07 -0600

    Implement img2img-based morph mode.

--- prompt_morph.py
+++ prompt_morph.py
@@ -1,4 +1,5 @@
 import math
+from operator import mod
 import os
 
 import gradio as gr
@@ -22,6 +23,11 @@ def prompt_at_t(weight_indexes, prompt_list, t):
         ]
     )
 
+# used to set denoise strength and cfg scaled by t 
+# TODO: options for sigmoid, parabolic, others? instead of linear
+def lerp_at_t(min_val, max_val, t):
+    diff = max_val - min_val
+    return ( min_val + ( t * diff ) )
 
 """
 Interpolate between two (or more) prompts and create an image at each step.
@@ -35,14 +41,27 @@ class Script(scripts.Script):
 
     def ui(self, is_img2img):
         i1 = gr.HTML("<p style=\"margin-bottom:0.75em\">Keyframe Format: <br>Seed | Prompt or just Prompt</p>")
+        #TODO: Figure out elegant way to select txt2img or img2img separately for each pair of prompts.
+        #Also TODO: Figure out how to do tooltips instead of putting walls of text in the label
+        mode_select = gr.Radio(choices=["txt2img morph", "img2img morph"], value="txt2img morph", label="Type of morph to generate.\n\
+                                                                                                         txt2img morph will interpolate between two independently generated images - good for morphing between two pics with very different subjects.\n\
+                                                                                                         img2img morph will generate one image, then interpolate between the original and an img2img derivative of it - good for maintaining a consistent subject with only smaller details changed.")
         prompt_list = gr.TextArea(label="Prompt list", placeholder="Enter one prompt per line. Blank lines will be ignored.")
+        #TODO: Separate negative prompts (I've tried it, it works!)
         n_images = gr.Slider(minimum=2, maximum=256, value=25, step=1, label="Number of images between keyframes")
         save_video = gr.Checkbox(label='Save results as video', value=True)
         video_fps = gr.Number(label='Frames per second', value=5)
+        #TODO: Set i2i parameters visible/interactive based on mode selected
+        #TODO: Force min_denoise_str <= max_denoise_str, and min_i2i_cfg <= p.cfg
+        min_denoise_str = gr.Slider(minimum=0.0, maximum=1.0, value=0.4, step=0.01, label="Denoise strength for first step. Ignored if only 2 images requested.")
+        max_denoise_str = gr.Slider(minimum=0.0, maximum=1.0, value=0.9, step=0.01, label="Denoise strength for generating target image.")
+        alt_i2i_cfg = gr.Checkbox(value=False, label="Use alternate CFG scale for img2img gens")
+        gradual_cfg = gr.Checkbox(value=False, label="Gradually increase CFG scale - helps with preventing artifacting/'deepfrying' on img2img gens with low denoising strength.")
+        min_i2i_cfg = gr.Slider(minimum=1, maximum=30, value=7, label="CFG scale for first img2img step (ignored if only 2 images requested)")
+        max_i2i_cfg = gr.Slider(minimum=1, maximum=30, value=7, label="CFG scale for final img2img step (used for all i2i gens if gradual CFG not selected)")
+        return [i1, prompt_list, mode_select, n_images, save_video, video_fps, min_denoise_str, max_denoise_str, alt_i2i_cfg, gradual_cfg, min_i2i_cfg, max_i2i_cfg]
 
-        return [i1, prompt_list, n_images, save_video, video_fps]
-
-    def run(self, p, i1, prompt_list, n_images, save_video, video_fps):
+    def run(self, p, i1, prompt_list, mode_select, n_images, save_video, video_fps, min_denoise_str, max_denoise_str, alt_i2i_cfg, gradual_cfg, min_i2i_cfg, max_i2i_cfg):
         # override batch count and size
         p.batch_size = 1
         p.n_iter = 1
@@ -61,6 +80,10 @@ class Script(scripts.Script):
 
         if len(prompts) < 2:
             msg = "prompt_morph: at least 2 prompts required"
+            print(msg)
+            return Processed(p, [], p.seed, info=msg)
+        if len(prompts) > 2 and mode_select == "img2img morph":
+            msg = "prompt_morph: More than 2 prompts not yet supported for img2img mode."
             print(msg)
             return Processed(p, [], p.seed, info=msg)
 
@@ -84,6 +107,31 @@ class Script(scripts.Script):
         p.outpath_samples = morph_path
 
         all_images = []
+
+        if not alt_i2i_cfg:
+            max_i2i_cfg = p.cfg_scale
+        i2i_p = processing.StableDiffusionProcessingImg2Img(
+            sd_model=p.sd_model,
+            outpath_samples=p.outpath_samples,
+            outpath_grids=p.outpath_grids,
+            styles=p.styles,
+            seed=p.seed,
+            subseed=p.subseed,
+            subseed_strength=p.subseed_strength,
+            seed_resize_from_h=p.seed_resize_from_h,
+            seed_resize_from_w=p.seed_resize_from_w,
+            sampler_index=p.sampler_index,
+            batch_size=1,
+            n_iter=1,
+            steps=p.steps,
+            cfg_scale=max_i2i_cfg,
+            width=p.width,
+            height=p.height,
+            restore_faces=p.restore_faces,
+            tiling=p.tiling,
+            init_images=[]
+        )
+
         for n in range(1, len(prompts)):
             # parsed prompts
             start_seed, start_prompt = prompts[n-1]
@@ -122,11 +170,25 @@ class Script(scripts.Script):
                 p.prompt = f'{scaled_prompt} AND {scaled_target}'
                 if p.seed != p.subseed:
                     p.subseed_strength = t
+                    i2i_p.subseed_strength = t
+                
+                if i == 0 or mode_select != "img2img morph":
+                    processed = process_images(p)
+                else:
+                    i2i_p.prompt = p.prompt
+                    if n_images > 2:
+                        i2i_t = (i - 1) / (n_images - 2)
+                    else:
+                        i2i_t = 1.0
+                    if gradual_cfg:
+                        i2i_p.cfg_scale = lerp_at_t(min_i2i_cfg, max_i2i_cfg, i2i_t)
+                    i2i_p.denoising_strength = lerp_at_t(min_denoise_str, max_denoise_str, i2i_t)
+                    processed = process_images(i2i_p)
 
-                processed = process_images(p)
                 if not state.interrupted:
                     all_images.append(processed.images[0])
-
+                    if i == 0 and mode_select == "img2img morph":
+                        i2i_p.init_images.append(all_images[0])
         if save_video:
             clip = ImageSequenceClip.ImageSequenceClip([np.asarray(t) for t in all_images], fps=video_fps)
             clip.write_videofile(os.path.join(morph_path, f"morph-{morph_number:05}.webm"), codec='libvpx-vp9', ffmpeg_params=['-pix_fmt', 'yuv420p', '-crf', '32', '-b:v', '0'], logger=None)
